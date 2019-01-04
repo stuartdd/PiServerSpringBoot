b25301761da9d665c293a1f377ef40ba6b1eb244
Stuart <stuart.d.davies@btinternet.com>
2019-01-04 23:36:06 +0000

    Update runWebApp.sh

--- runWebApp.sh
+++ runWebApp.sh
@@ -2,7 +2,7 @@
 RC=2
 while [ $RC -eq 2 ]
 do
-	LOGFILE=/logs/netty_`date +%Y_%m_%d_%H_%M_%S`.log
+	LOGFILE=/logs/sb_`date +%Y_%m_%d_%H_%M_%S`.log
 	java -jar PiServerSpringBoot-1.0.jar configWebApp.json 2> $LOGFILE
 	RC=$?
 done
