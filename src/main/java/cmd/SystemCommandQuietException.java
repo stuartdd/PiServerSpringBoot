38e82aa72dbf3d631bc39e8cd4275e4b807ea8cb
stuartdd <stuart.d.davies@btinternet.com>
2019-01-10 23:57:57 +0000

    functions

---
+++ src/main/java/cmd/SystemCommandQuietException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmd;
+
+import exceptions.ServerRestException;
+import org.springframework.http.HttpStatus;
+
+/**
+ *
+ * @author stuar
+ */
+public class SystemCommandException extends ServerRestException {
+
+    public SystemCommandException(String message) {
+        super(message, HttpStatus.FAILED_DEPENDENCY, "Failed Dependency");
+
+    }
+
+    public SystemCommandException(String message, Throwable cause) {
+        super(message, HttpStatus.FAILED_DEPENDENCY, "Failed Dependency", cause);
+    }
+
+}
