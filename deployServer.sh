1da0e7383896fe1e043e4072e259d538b69d0953
stuartdd <stuart.d.davies@btinternet.com>
2019-06-13 07:53:44 +0100

    deploy

--- deployServer.sh
+++ deployServer.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
+#
+# https://www.tecmint.com/sftp-command-examples/
+#
 set -e
 
-sshpass -v -p ims2d sftp -oBatchMode=no -b depolyServer.script pi@topbox
+sshpass -v -p $1 sftp -oBatchMode=no -b depolyServer.script pi@topbox
 
 curl -X GET http://topbox:8080/server/restart
