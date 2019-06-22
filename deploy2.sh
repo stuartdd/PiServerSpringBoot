65d60bd04d6278ac85a35558d4c7f55db76ba503
stuartdd <stuart.d.davies@btinternet.com>
2019-06-22 09:20:42 +0100

    startud

---
+++ deploy2.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Examples of commands:
+# https://www.tecmint.com/sftp-command-examples/
+#
+# To remove the need for a password:
+# https://www.maketecheasier.com/run-automated-scripts-ssh/
+#
+# set -e prevents the script from continuing if an error occurs!
+#
+set -e
+#
+# Copy resources to the pi
+#
+sftp -b depolyServer.script pi@topbox
+#
+# Restart the server
+#
+curl -X GET http://topbox:8080/server/restart
